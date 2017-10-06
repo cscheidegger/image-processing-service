62c93cf96fb17a33f4fe613d2342bc899ec2d09d
Vitor George <vitor.george@aparabolica.com.br>
2017-10-06 12:19:29 -0300

    Add parenthesis to print
    Former-commit-id: 28e2bf064c241a10506fc08a3a2b9fe0059509cb

--- Application.py
+++ Application.py
@@ -11,15 +11,15 @@ import Detection as detect
 import Utils
 import Training
 import Classification
-import types
+import types  
 import IO
 import Defects
 
-print "Process started"
+print("Process started")
 
 # image of palette
 imname = '06.4SEM.CENC.INTRA.SONY.jpg'
-im = cv2.imread("/home/joaoherrera/Pictures/AeTrapp/Sony/" + imname, 1)
+im = cv2.imread("/src/samples/" + imname, 1)
 
 im = Utils.adjust_position(im)
 im = Utils.adjust_resolution(im)
@@ -37,16 +37,16 @@ if type(im) == type(""):
 
 if Defects.isBlurred(im) < 17.0:
 	error = "ERROR: Blurred image!"
-	print error
+	print(error)
 	exit()
 
 if Defects.isDark(im) < 130.0:
 	error = "ERROR: The image is too dark!"
-	print error
+	print(error)
 	exit()
 
 
-print "Detecting circle...\n"
+print("Detecting circle...\n")
 
 # detecting the central circle
 # The program will try to recognize the central circle in 3 attempts.
@@ -58,7 +58,7 @@ for att in range(3):
 
 	if type(params) == type(None):
 		error = "ERROR: Failed to recognize the central circle."
-		print error
+		print(error)
 
 	if att == 2:
 		exit()
@@ -75,11 +75,11 @@ gsimage = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 # hinder the egg recognition
 if Defects.shadow_index(im) > 0.13:
 	error = "ERROR: The image has shadows or something else that might hinder the egg recognition"
-	print error
+	print(error)
 	exit()
 
 
-print "Performing segmentation..."
+print("Performing segmentation...")
 
 
 # detect borders using canny
@@ -92,7 +92,7 @@ bimage = img_as_ubyte(bimage) # converting image format to unsigned byte
 # ============================================================== BORDER SIZE
 # get perimeter of everything is in the image
 
-print "Performing border detection..."
+print("Performing border detection...")
 objects = detect.object_detection(bimage)
 
 #Training.border_lenght(bimage, objects, params[2])
@@ -105,7 +105,7 @@ eggs, clusters = Classification.border_lenght_classification(objects, params[2])
 # ============================================================== BORDER SHAPE
 # checking the shapes of eggs...
 
-print "Performing shape detection...\n"
+print("Performing shape detection...\n")
 
 shapes = []
 for i in range(len(eggs)):
@@ -127,7 +127,7 @@ clusters = Classification.border_shape_classification(shapesc, clusters, "shcls.
 # ============================================================== BORDER COLOR
 # Get colors info of remaining objects...
 
-print "\nPerforming color detection..."
+print("\nPerforming color detection...")
 #IO._write_results(bimage, eggs, clusters)
 areas_egg = []
 areas_clusters = []
@@ -153,7 +153,7 @@ for i in range(len(clusters)):
 
 if len(clusters) > 0:
 	cluster_textures = []
-	print "\nExtracting cluster textures..."
+	print("\nExtracting cluster textures...")
 
 	imGrey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
@@ -186,7 +186,7 @@ for cluster in clusters:
 	total_eggs += np.round(float(cluster['lenght']) / float(eggs_size_avg))
 
 
-print str(total_eggs) + " eggs found."
+print(str(total_eggs) + " eggs found.")
 
 IO._write_results(bimage, eggs, clusters, imname)
 #io.imsave("/home/joaoherrera/Desktop/" + imname[:-4] + "_out.jpg", bimage)
