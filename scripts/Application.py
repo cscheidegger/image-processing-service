164f3d6c2756bf2c7e646307eb59ea469bef3467
João Herrera <joao.herrera@usp.br>
2017-11-27 21:28:59 +0800

    Blur parameter changed

--- scripts/Application.py
+++ scripts/Application.py
@@ -32,7 +32,7 @@ IO.set_outputs(im)
 
 # Check if the palette has a defect. Currently there are 6 implementations of defects analysis:
 # low brightness, missing borders, out of focus, shadows and water on the surface. Some of them are detected using deep learning algorithm.
-if Defects.isBlurred(im) < 17.0:
+if Defects.isBlurred(im) < 13.0:
 	print(IO.json_packing_error('ERR_001'))
 	exit()
 
